f479113358677717674983129e80419c36db095c
Andrei Istratii <andrei.istratii@gmail.com>
2014-06-18 04:55:58 +0300

    Solve line endings

--- source/battleship/application.cpp
+++ source/battleship/application.cpp
@@ -145,7 +145,7 @@ void Application::Render(double inFrame)
 	bb->FillWithColor(RGB(200, 200, 200));
 	HDC hDC = bb->GetDC();
 
-	DrawField(hDC, testField.GetCells());
+	//DrawField(hDC, testField.GetCells());
 
 
 	InvalidateRect(viewport.GetHWND(), NULL, FALSE);
