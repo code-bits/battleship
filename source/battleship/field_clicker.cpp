f8d1dbe0e89a70d8e9f39dc9964347fc0b873a18
Andrei Istratii <andrei.istratii@gmail.com>
2014-06-20 10:32:47 +0300

    Fix contsructor arguments

--- source/battleship/field_clicker.cpp
+++ source/battleship/field_clicker.cpp
@@ -1,10 +1,10 @@
 #include "field_clicker.h"
 #include "stdafx.h"
 
-FieldClicker::FieldClicker(int top, int left, int size)
-	: top(top),
-	  left(left),
-	  size(size)
+FieldClicker::FieldClicker(int t, int l, int s)
+	: top(t),
+	  left(l),
+	  size(s)
 {}
 
 bool FieldClicker::isInArea(int x, int y)
